206a33e9eb8e7079f8eeb9e722a3d253e819519b
n1t5ua5 <nitsua5@proton.me>
2024-09-13 14:23:49 -0400

    lyrics input

--- SongLyrics.java
+++ SongLyrics.java
@@ -1,7 +1,10 @@
 public class SongLyrics {
 
     public static void main(String[] args {
-        System.out.printIn("I need forgiveness, for what I'm about to do.");
-
+        System.out.printIn("When you spend enough time in hell, ");
+        System.out.printIn("it brings out the devil in you.");
+        System.out.printIn("I don't seek penance for what I've done.");
+        System.out.printIn("I seek forgiveness, for what I'm about to do.");
+        System.out.printIn("A World on Fire by Within the Ruins: 🎧 https://youtu.be/Afg0-QN_Ihg?si=eRty_5RqLD92DDqx 🎧 ")
     }
 }
