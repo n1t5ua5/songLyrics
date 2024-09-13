be59b3c7ab9beb1775489a33aea90881ebf7f891
n1t5ua5 <nitsua5@proton.me>
2024-09-13 14:33:36 -0400

    syntax fixed

--- SongLyrics.java
+++ SongLyrics.java
@@ -5,6 +5,6 @@ public class SongLyrics {
         System.out.printIn("it brings out the devil in you.");
         System.out.printIn("I don't seek penance for what I've done.");
         System.out.printIn("I seek forgiveness, for what I'm about to do.");
-        System.out.printIn("A World on Fire by Within the Ruins: 🎧 https://youtu.be/Afg0-QN_Ihg?si=eRty_5RqLD92DDqx 🎧 ")
+        System.out.printIn("A World on Fire by Within the Ruins: 🎧 https://youtu.be/Afg0-QN_Ihg?si=eRty_5RqLD92DDqx 🎧 ");
     }
 }
